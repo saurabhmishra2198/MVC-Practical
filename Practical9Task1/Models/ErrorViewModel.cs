62f8bb232c4faf4fda3401d2e15b855f696a761c
Saurabh mishra <sacin119811@gmail.com>
2023-05-31 22:43:39 +0530

    Practical9

---
+++ Practical9Task1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Practical9Task1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
